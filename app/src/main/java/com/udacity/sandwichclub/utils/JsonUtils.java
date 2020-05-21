1534c7b03ba06599bb01f12dd4b917020d164220
bwielk <bwielk@gmail.com>
2020-05-21 19:23:37 +0200

    Dealing with JSON - initial commit

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,20 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+        try{
+            JSONObject sandwichAsJson = new JSONObject(json);
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+        return sandwich;
     }
 }
+
+
